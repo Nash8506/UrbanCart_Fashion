aad2d5267f1e5c6f4b8219e7a72cfb89b3d2a383
Nashid K <kp.nashid.nashid@gmail.com>
2024-05-24 00:01:42 +0530

    main carousel added

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {mainCarouselData} from './MainCarouselData'
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const MainCarousel = () => {
+
+    const items = mainCarouselData.map((item) => <img className="cursor-pointer" role='presentation' src={item.imageSrc} alt="" />);
+
+
+  return (
+            <AliceCarousel
+                infinite
+                autoPlay
+                autoPlayInterval={2000}
+                animationDuration={1000}
+                disableButtonsControls
+                items={items}
+            />
+        );
+}
+
+export default MainCarousel
---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,17 @@
+export const mainCarouselData=[
+    {
+    imageSrc: 'https://m.media-amazon.com/images/G/31/img24/Fashion/AF/SS24/Flip/TopHero/PC/cml-pc._CB580671423_.jpg',
+    },
+    {
+    imageSrc: 'https://m.media-amazon.com/images/G/31/img24/Fashion/AF/SS24/Flip/TopHero/PC/genz-PC._CB580671423_.jpg',
+    },
+    {
+    imageSrc: 'https://m.media-amazon.com/images/G/31/img23/WA/2024/feb/SS/Top-Ingress-pc._SX3000_QL85_.jpg',
+    },
+    {
+    imageSrc: 'https://m.media-amazon.com/images/G/31/img23/Fashion/AF/JanART/tophero/Men-s-clothing_3000x800_21._SX3000_QL85_FMpng_.png',
+    },
+    {
+    imageSrc: 'https://m.media-amazon.com/images/G/31/img24/Fashion/Event/JanART/Eventpage/AF/stealdeals/banner/SD-Top-Herotator._CB585995939_.gif',
+    },
+]
